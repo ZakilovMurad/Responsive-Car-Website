223ff705845de7898a9f93863367137e6da9d216
ZakilovMurad <zakilovmurad9@gmail.com>
2025-05-26 21:37:33 +0400

    header bg change done

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,13 +34,20 @@ const swiperHome = new Swiper('.home__swiper', {
             return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + "</span>";
         },
     },
-    autoplay: {
-        delay: ,
-    }
+    // autoplay: {
+    //     delay: 3000,
+    // }
 })
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header');
+
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/
